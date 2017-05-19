a2d676b00febeadaa5afcddfb6d9adc5ba498e00
Emils Rits <rits.emils@gmail.com>
2017-05-19 16:46:58 +0300

    Remove links from checkout progress, restyle

--- resources/views/partials/widgets/checkout_progress.blade.php
+++ resources/views/partials/widgets/checkout_progress.blade.php
@@ -1,29 +1,24 @@
 <div class="checkout-progress">
     <ul class="cf">
         <li class="{{ $page >= 1 ? 'current' : ''}}">
-            <a href="{{ url('/cart') }}">
-                <span class="hidden-lg hidden-md hidden-sm">1</span><span class="hidden-xs">Cart</span>
-            </a>
+            <span class="hidden-lg hidden-md hidden-sm">1</span><span class="hidden-xs">Cart</span>
+            <i class="fa fa-angle-right" aria-hidden="true"></i>
         </li>
         <li class="{{ $page >= 2 ? 'current' : ''}}">
-            <a href="{{ url('/checkout') }}">
-                <span class="hidden-lg hidden-md hidden-sm">2</span><span class="hidden-xs">Shipping Information</span>
-            </a>
+            <span class="hidden-lg hidden-md hidden-sm">2</span><span class="hidden-xs">Shipping Information</span>
+            <i class="fa fa-angle-right" aria-hidden="true"></i>
         </li>
         <li class="{{ $page >= 3 ? 'current' : ''}}">
-            <a href="{{ url('/checkout/delivery') }}">
-                <span class="hidden-lg hidden-md hidden-sm">3</span><span class="hidden-xs">Delivery</span>
-            </a>
+            <span class="hidden-lg hidden-md hidden-sm">3</span><span class="hidden-xs">Delivery</span>
+            <i class="fa fa-angle-right" aria-hidden="true"></i>
         </li>
         <li class="{{ $page >= 4 ? 'current' : ''}}">
-            <a href="{{ url('/checkout/confirmation') }}">
-                <span class="hidden-lg hidden-md hidden-sm">4</span><span class="hidden-xs">Confirmation</span>
-            </a>
+            <span class="hidden-lg hidden-md hidden-sm">4</span><span class="hidden-xs">Confirmation</span>
+            <i class="fa fa-angle-right" aria-hidden="true"></i>
         </li>
         <li class="{{ $page >= 5 ? 'current' : ''}}">
-            <a href="{{ url('/checkout/success') }}">
-                <span class="hidden-lg hidden-md hidden-sm">5</span><span class="hidden-xs">Success</span>
-            </a>
+            <span class="hidden-lg hidden-md hidden-sm">5</span><span class="hidden-xs">Success</span>
+            <i class="fa fa-check-circle-o" aria-hidden="true"></i>
         </li>
     </ul>
 </div>
