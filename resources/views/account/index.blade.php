fd7a42c69f8f0fce7fe7413f9b6b66d4be9344bf
Emils Rits <rits.emils@gmail.com>
2017-05-22 04:07:54 +0300

    Add user account section navigation

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -5,13 +5,30 @@ My Account
 @endsection
 
 @section('content')
-<div class="lg-100 md-100 sm-100">
+<div class="grid rlg-100 md-100 sm-100">
     <div id="user-account">
         <h4>My Account</h4>
-        <div class="lg-100 md-100 sm-100">
-            <div class="lg-20 md-10 sm-10">
-
+        <div class="grid-uniform lg-100 md-100 sm-100">
+            <div id="account-nav">
+                <div class="account-settings grid-item lg-33 md-33 sm-33">
+                    <a href="{{ url('/user/edit') }}">
+                        <i class="fa fa-address-card" aria-hidden="true"></i> Update Account
+                    </a>
+                </div>
+                <div class="account-orders grid-item lg-33 md-33 sm-33">
+                    <a class="active" href="{{ url('/user/account') }}">
+                        <i class="fa fa-shopping-basket" aria-hidden="true"></i> My Orders
+                    </a>
+                </div>
+                <div class="account-order-history grid-item lg-33 md-33 sm-33">
+                    <a href="{{ url('/user/order/history') }}">
+                        <i class="fa fa-history" aria-hidden="true"></i> Order History
+                    </a>
+                </div>
             </div>
+        </div>
+        <div id="account-panel" class="grid-item lg-80 md-85 sm-85">
+
         </div>
     </div>
 </div>
