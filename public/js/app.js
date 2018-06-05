6504b43f0dea312519bc0a933de38e6be201bf07
Emils Rits <rits.emils@gmail.com>
2018-06-05 19:34:28 +0300

    Switch to Laravel mix

--- public/js/app.js
+++ public/js/app.js
@@ -1,50 +1,49 @@
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
-
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
-
+/******/
 /******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId])
+/******/ 		if(installedModules[moduleId]) {
 /******/ 			return installedModules[moduleId].exports;
-
+/******/ 		}
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
 /******/ 			i: moduleId,
 /******/ 			l: false,
 /******/ 			exports: {}
 /******/ 		};
-
+/******/
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-
+/******/
 /******/ 		// Flag the module as loaded
 /******/ 		module.l = true;
-
+/******/
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
 /******/ 	}
-
-
+/******/
+/******/
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
-
+/******/
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
-
-/******/ 	// identity function for calling harmory imports with the correct context
-/******/ 	__webpack_require__.i = function(value) { return value; };
-
-/******/ 	// define getter function for harmory exports
+/******/
+/******/ 	// define getter function for harmony exports
 /******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		Object.defineProperty(exports, name, {
-/******/ 			configurable: false,
-/******/ 			enumerable: true,
-/******/ 			get: getter
-/******/ 		});
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
 /******/ 	};
-
+/******/
 /******/ 	// getDefaultExport function for compatibility with non-harmony modules
 /******/ 	__webpack_require__.n = function(module) {
 /******/ 		var getter = module && module.__esModule ?
@@ -53,31 +52,39 @@
 /******/ 		__webpack_require__.d(getter, 'a', getter);
 /******/ 		return getter;
 /******/ 	};
-
+/******/
 /******/ 	// Object.prototype.hasOwnProperty.call
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-
+/******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-
+/******/ 	__webpack_require__.p = "/";
+/******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-(function($) {
+__webpack_require__(1);
+module.exports = __webpack_require__(2);
 
-    $(document).ready(function() {
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+(function ($) {
+
+    $(document).ready(function () {
         // Hide flash message
         $('.message-close').css('display', 'inline-block').click(function () {
             $('.flash-message').fadeOut(500);
         });
 
         // Check all checkboxes in catalog for mass action
-        $('#mass-select').click(function() {
+        $('#mass-select').click(function () {
             if ($(this).is(':checked')) {
                 $('.entity-select').each(function () {
                     $(this).prop('checked', true);
@@ -90,7 +97,7 @@
         });
 
         // Confirm product deletion in catalog
-        $('#catalog-form').submit(function() {
+        $('#catalog-form').submit(function () {
             var massAction = $('#mass-action').val();
             if (massAction === '3') {
                 var numberOfChecked = $('.entity-select:checked').length;
@@ -102,27 +109,27 @@
         });
 
         // Confirm entity delete
-        $('#entity-delete').click(function() {
+        $('#entity-delete').click(function () {
             return confirm('Delete this?');
         });
 
         // Toggle specifications sections when creating product
-        $('.content-section-toggle').click(function() {
+        $('.content-section-toggle').click(function () {
             $('i', this).toggleClass('fa-angle-up fa-angle-down');
             $(this).parent().find('.content-container').slideToggle();
         });
 
         // Toggle parent_id selection for category creation
-        $('#category-parent').change(function() {
-           if ($(this).val() === '1') {
-               $('#category-parent-id').hide();
-           } else {
-               $('#category-parent-id').show();
-           }
+        $('#category-parent').change(function () {
+            if ($(this).val() === '1') {
+                $('#category-parent-id').hide();
+            } else {
+                $('#category-parent-id').show();
+            }
         });
 
         // Confirm category deletion in categories view
-        $('#categories-form').submit(function() {
+        $('#categories-form').submit(function () {
             var massAction = $('#mass-action').val();
             if (massAction === '3') {
                 var numberOfChecked = $('.entity-select:checked').length;
@@ -134,7 +141,7 @@
         });
 
         // Confirm specification deletion in specifications view
-        $('#specifications-form').submit(function() {
+        $('#specifications-form').submit(function () {
             var massAction = $('#mass-action').val();
             if (massAction === '1') {
                 var numberOfChecked = $('.entity-select:checked').length;
@@ -146,7 +153,7 @@
         });
 
         // Confirm attribute deletion
-        $('#attributes-form').submit(function() {
+        $('#attributes-form').submit(function () {
             var massAction = $('#mass-action').val();
             if (massAction === '1') {
                 var numberOfChecked = $('.entity-select:checked').length;
@@ -158,7 +165,7 @@
         });
 
         // Confirm user deletion
-        $('#users-form').submit(function() {
+        $('#users-form').submit(function () {
             var massAction = $('#mass-action').val();
             if (massAction === '2') {
                 var numberOfChecked = $('.entity-select:checked').length;
@@ -170,24 +177,24 @@
         });
 
         // Uncheck other checkboxes when one of delivery options is selected
-        $('.checkout-delivery-storage, .checkout-delivery-address').click(function() {
+        $('.checkout-delivery-storage, .checkout-delivery-address').click(function () {
             var checkbox = $(this).find('input[type="checkbox"]');
             checkbox.prop('checked', true);
             $('input[type="checkbox"]').not(checkbox).prop('checked', false);
         });
 
         // Disable order submit button after once click
-        $('#checkout-confirm-form').one('submit', function() {
-            $(this).find('#order-submit').css('opacity', '0.6').attr('onclick','return false;');
+        $('#checkout-confirm-form').one('submit', function () {
+            $(this).find('#order-submit').css('opacity', '0.6').attr('onclick', 'return false;');
         });
 
         // Clear all filters when clicked
-        $('#filters-clear').click(function() {
+        $('#filters-clear').click(function () {
             clearAllInputs($('#table-search'));
         });
         // Clear all filters from a admin panel table
         function clearAllInputs(selector) {
-            $(selector).find(':input').each(function() {
+            $(selector).find(':input').each(function () {
                 $(this).val('');
             });
             $(selector).find('select').each(function () {
@@ -196,14 +203,14 @@
         }
 
         // Toggle dropdown for category navigation
-        $('.dropdown-trigger').click(function() {
+        $('.dropdown-trigger').click(function () {
             toggleDropdown($(this));
         });
         function toggleDropdown(selector) {
             $('.dropdown-content').hide();
             $(selector).find('.dropdown-content').show();
         }
-        $(document).on('click', function(e) {
+        $(document).on('click', function (e) {
             if (!$(e.target).closest('.dropdown-content').length && !$(e.target).closest('.dropdown-trigger').length) {
                 $('.dropdown-content').hide();
             }
@@ -211,24 +218,24 @@
 
         /* ------AJAX------ */
         // Load category specifications when creating product
-        $('#category-select').change(function() {
+        $('#category-select').change(function () {
             $.ajax({
                 type: 'GET',
                 url: '/admin/product/categories',
                 data: {
                     selectFieldValue: $(this).val()
                 },
-                success: function(data) {
+                success: function success(data) {
                     window.location.href = data.redirectUrl + data.category;
                 },
-                error: function(err) {
+                error: function error(err) {
                     console.log("error: " + err);
                 }
             });
         });
 
         // Add a product to cart
-        $('.product-quick-add').click(function() {
+        $('.product-quick-add').click(function () {
             $.ajax({
                 type: 'GET',
                 url: '/cart/add/' + $(this).val(),
@@ -236,17 +243,22 @@
                     productId: $(this).val()
                 },
                 dataType: 'json',
-                success: function(data) {
+                success: function success(data) {
                     $('#navbar-cart-items').html(data);
                 },
-                error: function(err) {
+                error: function error(err) {
                     console.log("error: " + err);
                 }
             });
         });
-    })
+    });
+})(jQuery);
 
-}(jQuery));
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
 
-/***/ }
+// removed by extract-text-webpack-plugin
+
+/***/ })
 /******/ ]);
